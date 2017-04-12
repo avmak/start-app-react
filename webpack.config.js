d2631325a0b842304829ba831913da1918040ece
av_mak <alexey.makarov@smartbics.com>
2017-04-12 11:59:19 +0300

    features: refactoring structure and code

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
 
-  entry: './app/components/Main.js',
-  
+  entry: './app/index.js',
+
   output: {
-    filename: 'public/bundle.js'
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
   },
-  
+
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader'
-        ]
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [ 'babel-loader' ],
+      },
+      {
+        test: /\.jsx$/,
+        exclude: [/node_modules/],
+        use: [ 'babel-loader' ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       }
     ]
-  }
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    modules: [
+      'node_modules'
+    ]
+  },
+
+  plugins: [HtmlWebpackPluginConfig]
+
 };
